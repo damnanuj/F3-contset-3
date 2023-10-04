c13f6065a38d8abad13f1be14473717eb6296dab
Anuj <anujkrgupta21@gmail.com>
2023-10-05 00:24:58 +0530

    added login form

--- script.js
+++ script.js
@@ -9,11 +9,15 @@
 const logBtn = document.querySelector(".btn");
 const formContainer = document.querySelector(".formContainer");
 const hider =document.querySelector(".xmark");
+const blurEffect =  document.getElementById("blurBackground");
 
 logBtn.addEventListener("click", () => {
     formContainer.classList.add("popupShow")
+    blurEffect.style.display = "block";
 });
 
 hider.addEventListener("click", () => {
     formContainer.classList.remove("popupShow")
+    blurEffect.style.display = "none";
 })
+
