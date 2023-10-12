4f99f7b86a634d4dd68087774c33f336bdcf8deb
Anuj <anujkrgupta21@gmail.com>
2023-10-12 16:06:15 +0530

    added dynamic food items and json

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 
 const logBtn = document.querySelector(".btn");
 const formContainer = document.querySelector(".formContainer");
-const hider =document.querySelector(".xmark");
+const hider = document.querySelector(".xmark");
 const blurEffect =  document.getElementById("blurBackground");
 
 logBtn.addEventListener("click", () => {
@@ -21,3 +21,39 @@ hider.addEventListener("click", () => {
     blurEffect.style.display = "none";
 })
 
+
+
+async function fetchData() {
+    const resp = await fetch(
+      "https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json"
+    );
+    const data = await resp.json();
+    // console.log(data);
+    var container = "";
+    data.forEach((item) => {
+      container += `
+      <div class="cards" id=${item.id}>
+              <img src=${item.imgSrc} width="200px" height="100px"
+              alt="food image" />
+              <div class="priceName">
+                <div class="price">
+                  <h3>${item.name}</h3>
+                  <p>$${item.price}/-</p>
+                </div>
+                <div class="addCart">
+                  <i class="fa-solid fa-plus"></i>
+                </div>
+              </div>
+            </div>`;
+
+    
+
+
+
+    });
+    // console.log(container);
+    document.querySelector(".cardsContainer").innerHTML = container;
+  }
+  
+  fetchData();
+  
